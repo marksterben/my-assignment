760aec512630bdbbc063b64f5b0ce3560f4cccec
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-19 23:53:11 +0700

    create searchbar component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import styles from "./SearchBar.module.css";
+
+const SearchBar = () => {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search.."
+        className={styles.searchInput}
+      />
+      <button type="button" className={styles.searchBtn}>
+        <i class="fas fa-search fa-rotate-90"></i>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
