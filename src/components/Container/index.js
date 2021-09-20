3577d483e9a2626853460e95f69352ba84713665
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-20 15:30:17 +0700

    Update index.js

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styles from "./Container.module.css";
 
 const Container = ({ children }) => {
-  return <div className={styles.Container}>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 };
 
 Container.propTypes = {
