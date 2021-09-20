28616f970f59a4a3c4af58e602591d58e3dd05ac
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-20 15:46:08 +0700

    fix font bug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import "./assets/fonts/fontawesome-webfont.ttf";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
