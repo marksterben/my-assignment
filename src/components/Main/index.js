6a9a0e2f9b532f575c9dd7465011cce5e80de01c
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-21 13:09:50 +0700

    Update index.js

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,21 @@
+import PropTypes from "prop-types";
+
 import Body from "../Body";
 import Header from "../Header";
 
 import styles from "./Main.module.css";
 
-const Main = () => {
+const Main = ({ sidebarHandler }) => {
   return (
     <div className={styles.main}>
-      <Header />
+      <Header sidebarHandler={sidebarHandler} />
       <Body />
     </div>
   );
+};
+
+Main.propTypes = {
+  sidebarHandler: PropTypes.func.isRequired,
 };
 
 export default Main;
