dd3deabd782c4b05adec2d3407f6f8177fab5bdb
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-21 12:56:58 +0700

    improve featurecard hover

--- src/components/FeatureCard/index.js
+++ src/components/FeatureCard/index.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 import PropTypes from "prop-types";
 
 import styles from "./FeatureCard.module.css";
 
-const FeatureCard = ({ title, text, image }) => {
+const FeatureCard = ({ title, text, image, imageHover }) => {
+  const [hover, setHover] = useState(false);
+
+  const hoverHandler = () => {
+    setHover(!hover);
+  };
+
   return (
-    <div className={styles.featureCard}>
-      <img className={styles.cardImg} src={image} alt="" />
+    <div
+      onMouseEnter={hoverHandler}
+      onMouseLeave={hoverHandler}
+      className={styles.featureCard}
+    >
+      <img className={styles.cardImg} src={hover ? imageHover : image} alt="" />
       <p className={styles.cardTitle}>{title}</p>
       <p className={styles.cardText}>{text}</p>
     </div>
