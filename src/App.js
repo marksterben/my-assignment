217bbdc22b89df632fa37b858c7f7c38082c1e49
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-21 13:38:36 +0700

    create sidebar toggle function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 import Container from "./components/Container";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 const App = () => {
-  return <Container></Container>;
+  const [click, setClick] = useState(false);
+
+  const sidebarHandler = () => {
+    setClick(!click);
+  };
+
+  return (
+    <Container>
+      <Navbar click={click} />
+      <Main sidebarHandler={sidebarHandler} />
+    </Container>
+  );
 };
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,24 @@
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
 import SearchBar from "../SearchBar";
 import Nav from "../Nav";
 import NavFeature from "../NavFeature";
 
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = ({ click }) => {
   return (
-    <nav className={styles.navbar}>
+    <nav className={classNames(styles.navbar, { [styles.toggle]: click })}>
       <SearchBar />
       <Nav />
       <NavFeature />
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  click: PropTypes.bool.isRequired,
 };
 
 export default Navbar;
